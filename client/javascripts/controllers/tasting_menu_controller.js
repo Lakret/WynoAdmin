a9cb10ac47892de2bc8a178e30fc678285119ca7
Alexander Hurst <123alekhurst@gmail.com>
2015-08-16 15:16:23 -0700

    quick links

--- client/javascripts/controllers/tasting_menu_controller.js
+++ client/javascripts/controllers/tasting_menu_controller.js
@@ -5,5 +5,8 @@ angular.module( 'WynoAdmin' ).controller( 'TastingMenuController', [
 '$location',
 '$rootScope',
 function( $scope, $stateParams, $http, $location, $rootScope ) {
-
+	$scope.goTo = function( location ) {
+		if( location === 'winery_home' )
+			$location.path( '/winery/0' );
+	}
 } ] );
